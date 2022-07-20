9aae1f6bcf86d6f9757aadcc5a212ce11ce45964
WayneWei228 <waynewei0228@gmail.com>
2022-07-20 13:36:12 -0700

    Initial Commit

---
+++ CG/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CG
+//
+//  Created by Wayne on 2022-07-20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
