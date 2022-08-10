628da0d3be34182a56402aefc96280fb0c8fca70
WayneWei228 <waynewei0228@gmail.com>
2022-08-09 23:45:13 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,45 +1,47 @@
 #ifndef _PGM_H_
 #define _PGM_H_
-#include <fstream>
-#include <iostream>
+#include <stdio.h>
+
 #include <algorithm>
-#include <string>
+#include <bitset>　/*std::bitset<32>*/
+#include <cmath>
 #include <cstdint>
+#include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <bitset>　/*std::bitset<32>*/
-#include <cstdio>
-#include <string>
-#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <limits>
 #include <queue>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
-#include <stdio.h>
 using namespace std;
 
-enum PFM_endianness { BIG, LITTLE, ERROR};
+enum PFM_endianness { BIG, LITTLE, ERROR };
 
 class PFM {
-public:
+   public:
     PFM() = default;
-    inline bool is_little_big_endianness_swap(){
+    inline bool is_little_big_endianness_swap() {
         if (this->endianess == 0.f) {
             std::cerr << "this-> endianness is not assigned yet!\n";
             exit(0);
-        }
-        else {
+        } else {
             uint32_t endianness = 0xdeadbeef;
-            unsigned char * temp = (unsigned char *)&endianness;
-            PFM_endianness endianType_ = ((*temp) ^ 0xef == 0 ?
-                                          LITTLE : (*temp) ^ (0xde) == 0 ? BIG : ERROR);
-            return  ((BIG == endianType_) && (this->endianess < 0.f))
-            || ((LITTLE == endianType_) && (this->endianess > 0.f));
+            unsigned char* temp = (unsigned char*)&endianness;
+            PFM_endianness endianType_ = ((*temp) ^ 0xef == 0     ? LITTLE
+                                          : (*temp) ^ (0xde) == 0 ? BIG
+                                                                  : ERROR);
+            return ((BIG == endianType_) && (this->endianess < 0.f)) ||
+                   ((LITTLE == endianType_) && (this->endianess > 0.f));
         }
     }
-    
-    
-    template<typename T>
-    T * read_pfm(const std::string & filename) {
-        FILE * pFile;
+
+    template <typename T>
+    T* read_pfm(const std::string& filename) {
+        FILE* pFile;
         pFile = fopen(filename.c_str(), "rb");
         char c[100];
         if (pFile != NULL) {
@@ -53,81 +55,72 @@ public:
                 int length_ = this->width * this->height;
                 fscanf(pFile, "%s", c);
                 this->endianess = atof(c);
-                
+
                 fseek(pFile, 0, SEEK_END);
                 long lSize = ftell(pFile);
-                long pos = lSize - this->width*this->height * sizeof(T);
+                long pos = lSize - this->width * this->height * sizeof(T);
                 fseek(pFile, pos, SEEK_SET);
-                
+
                 T* img = new T[length_];
-                //cout << "sizeof(T) = " << sizeof(T);
+                // cout << "sizeof(T) = " << sizeof(T);
                 fread(img, sizeof(T), length_, pFile);
                 fclose(pFile);
-                
-                T* tbimg = (T *)malloc(length_ * sizeof(T));// top-to-bottom.
-                //PFM SPEC image stored bottom -> top reversing image
+
+                T* tbimg = (T*)malloc(length_ * sizeof(T));  // top-to-bottom.
+                // PFM SPEC image stored bottom -> top reversing image
                 for (int i = 0; i < this->height; i++) {
-                    memcpy(&tbimg[(this->height - i - 1)*(this->width)],
-                           &img[(i*(this->width))],
-                           (this->width) * sizeof(T));
+                    memcpy(&tbimg[(this->height - i - 1) * (this->width)],
+                           &img[(i * (this->width))], (this->width) * sizeof(T));
                 }
-                
-                
-                if (this->is_little_big_endianness_swap()){
-                    std::cout << "little-big endianness transformation is needed.\n";
+
+                if (this->is_little_big_endianness_swap()) {
+                    std::cout << "little-big endianness transformation "
+                                 "is needed.\n";
                     // little-big endianness transformation is needed.
                     union {
                         T f;
                         unsigned char u8[sizeof(T)];
                     } source, dest;
-                    
+
                     for (int i = 0; i < length_; ++i) {
                         source.f = tbimg[i];
                         for (unsigned int k = 0, s_T = sizeof(T); k < s_T; k++)
                             dest.u8[k] = source.u8[s_T - k - 1];
                         tbimg[i] = dest.f;
-                        //cout << dest.f << ", ";
+                        // cout << dest.f << ", ";
                     }
                 }
                 delete[] img;
                 return tbimg;
-                
-            }
-            else {
+
+            } else {
                 std::cout << "Invalid magic number!"
-                << " No Pf (meaning grayscale pfm) is missing!!\n";
+                          << " No Pf (meaning grayscale pfm) is missing!!\n";
                 fclose(pFile);
                 exit(0);
             }
-            
-        }
-        else {
-            std::cout << "Cannot open file " << filename
-            << ", or it does not exist!\n";
+
+        } else {
+            std::cout << "Cannot open file " << filename << ", or it does not exist!\n";
             fclose(pFile);
             exit(0);
         }
-        
     }
-    
-    template<typename T>
-    void write_pfm(const std::string & filename, const T* imgbuffer,
-                   const float & endianess_) {
+
+    template <typename T>
+    void write_pfm(const std::string& filename, const T* imgbuffer, const float& endianess_) {
         std::ofstream ofs(filename.c_str(), std::ifstream::binary);
-        
-        ofs << "Pf\n"
-        << this->width << " " << this->height << "\n"
-        << endianess_ << "\n";
-        int length_ = this->width*this->height;
+
+        ofs << "Pf\n" << this->width << " " << this->height << "\n" << endianess_ << "\n";
+        int length_ = this->width * this->height;
         this->endianess = endianess_;
-        T* tbimg = (T *)malloc(length_ * sizeof(T));
+        T* tbimg = (T*)malloc(length_ * sizeof(T));
         // PFM SPEC image stored bottom -> top reversing image
         for (int i = 0; i < this->height; i++) {
-            memcpy(&tbimg[(this->height - i - 1)*this->width],
-                   &imgbuffer[(i*this->width)],
+            memcpy(&tbimg[(this->height - i - 1) * this->width], &imgbuffer[(i * this->width)],
                    this->width * sizeof(T));
         }
-        
+
         if (this->is_little_big_endianness_swap()) {
             std::cout << "little-big endianness transformation is needed.\n";
             // little-big endianness transformation is needed.
@@ -135,27 +128,28 @@ public:
                 T f;
                 unsigned char u8[sizeof(T)];
             } source, dest;
-            
+
             for (int i = 0; i < length_; ++i) {
                 source.f = tbimg[i];
                 for (size_t k = 0, s_T = sizeof(T); k < s_T; k++)
                     dest.u8[k] = source.u8[s_T - k - 1];
                 tbimg[i] = dest.f;
-                //cout << dest.f << ", ";
+                // cout << dest.f << ", ";
             }
         }
-        
-        ofs.write((char *)tbimg, this->width*this->height * sizeof(T));
+
+        ofs.write((char*)tbimg, this->width * this->height * sizeof(T));
         ofs.close();
         free(tbimg);
     }
-    
-    inline float getEndianess(){return endianess;}
-    inline int getHeight(void){return height;}
-    inline int getWidth(void){return width;}
-    inline void setHeight(const int & h){height = h;}
-    inline void setWidth(const int & w){width = w;}
-private:
+
+    inline float getEndianess() { return endianess; }
+    inline int getHeight(void) { return height; }
+    inline int getWidth(void) { return width; }
+    inline void setHeight(const int& h) { height = h; }
+    inline void setWidth(const int& w) { width = w; }
+
+   private:
     int height;
     int width;
     float endianess;
@@ -165,25 +159,31 @@ int Next8[8][2] = {{0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}, {1, 0},
 int Next4[4][2] = {{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
 #endif /* PGM_H_ */
 
-
 struct Coordinate {
     int X, Y;
+
+    bool operator==(const Coordinate& that) const { return X == that.X && Y == that.Y; }
+};
+
+struct CoordinateHash {
+    size_t operator()(const Coordinate& that) const { return that.X * size_t(9875321) + that.Y; }
 };
 
 struct Solution {
     PFM pfm_rw;
-    string path_in = "/Users/wayne_tx/Desktop/CG/onesquare_in.pfm";
+    string path_in = "/Users/wayne_tx/Desktop/CG/singlesharp_in.pfm";
     string path_out_C1 = "/Users/wayne_tx/Desktop/CG/onesquare_out_m1.pfm";
     string path_out_C2 = "/Users/wayne_tx/Desktop/CG/onesquare_out_m2.pfm";
     string path_out_C2_r2 = "/Users/wayne_tx/Desktop/CG/onesquare_out_m2_r2.pfm";
     string path_out_C2_r3 = "/Users/wayne_tx/Desktop/CG/onesquare_out_m2_r3.pfm";
+    string path_out_jfa = "/Users/wayne_tx/Desktop/CG/Complex/singlesharp_out_jfa.pfm";
     float* input = pfm_rw.read_pfm<float>(path_in);
     float* output = NULL;
     int imgH = pfm_rw.getHeight();
     int imgW = pfm_rw.getWidth();
     int size = imgH * imgW;
     float norm = sqrt(pow(imgH, 2) + pow(imgW, 2));
-    
+
     void ResetOut() {
         delete output;
         output = new float[imgH * imgW * sizeof(float)];
@@ -191,7 +191,7 @@ struct Solution {
             output[i] = 1.0;
         }
     }
-    
+
     void Display(float* in) {
         for (int i = 0; i < imgH; i++) {
             for (int j = 0; j < imgW; j++) {
@@ -200,32 +200,49 @@ struct Solution {
             cout << endl;
         }
     }
-    
-    void Solve() {
-        freopen("/Users/wayne_tx/Desktop/CG/CG/CG/ot.txt", "w", stdout);
-        cout <<" path in " << path_in << endl;
-        cout << "Height " << imgH << " Width " << imgW << endl;
-        ResetOut();
-        Cal1();
-        pfm_rw.write_pfm<float>(path_out_C1, output, -1.0f);
-        ResetOut();
-        Cal2(1);
-        pfm_rw.write_pfm<float>(path_out_C2, output, -1.0f);
-        ResetOut();
-        Cal2(2);
-        pfm_rw.write_pfm<float>(path_out_C2_r2, output, -1.0f);
-        ResetOut();
-        Cal2(3);
-        pfm_rw.write_pfm<float>(path_out_C2_r3, output, -1.0f);
-        fclose(stdout);
+
+    void InitialInput() {
+        for (int i = 0; i < imgH; i++) {
+            for (int j = 0; j < imgW; j++) {
+                if (input[i * imgW + j] < 0.8) {
+                    input[i * imgW + j] = 0;
+                } else {
+                    input[i * imgW + j] = 1.0;
+                }
+            }
+        }
     }
-    
-    void Cal1(){
+
+    void Solve() {
+        // freopen("/Users/wayne_tx/Desktop/CG/ot.txt", "w", stdout);
+        cout << " path in " << path_in << endl;
+        cout << "Height " << imgH << " Width " << imgW << endl;
+        InitialInput();
+        pfm_rw.write_pfm<float>(path_in, input, -1.0f);
+        // ResetOut();
+        // Cal1();
+        // pfm_rw.write_pfm<float>(path_out_C1, output, -1.0f);
+        // ResetOut();
+        // Cal2(1);
+        // pfm_rw.write_pfm<float>(path_out_C2, output, -1.0f);
+        // ResetOut();
+        // Cal2(2);
+        // pfm_rw.write_pfm<float>(path_out_C2_r2, output, -1.0f);
+        // ResetOut();
+        // Cal2(3);
+        // pfm_rw.write_pfm<float>(path_out_C2_r3, output, -1.0f);
+        ResetOut();
+        JFA();
+        pfm_rw.write_pfm<float>(path_out_jfa, output, -1.0f);
+        // fclose(stdout);
+    }
+
+    void Cal1() {
         for (int i = 0; i < imgH; i++) {
             for (int j = 0; j < imgW; j++) {
                 for (int x = 0; x < imgH; x++) {
                     for (int y = 0; y < imgW; y++) {
-                      if (input[x * imgW + y] == 0) {
+                        if (input[x * imgW + y] == 0) {
                             float dis = sqrt(pow(i - x, 2) + pow(j - y, 2)) / norm;
                             if (dis < output[i * imgW + j]) {
                                 output[i * imgW + j] = dis;
@@ -236,9 +253,7 @@ struct Solution {
             }
         }
     }
-    
 
-    
     void FloodFill(int x, int y, int r) {
         vector<bool> visited;
         visited.resize(imgH * imgW);
@@ -260,7 +275,7 @@ struct Solution {
                         output[x * imgW + y] = dis;
                     }
                 }
-                for (int i = 0; i < 8; i++) { // 加入新的点到newq
+                for (int i = 0; i < 8; i++) {  // 加入新的点到newq
                     int newX = front.X + Next8[i][0];
                     if (newX < 0 || newX >= imgH) continue;
                     int newY = front.Y + Next8[i][1];
@@ -286,10 +301,66 @@ struct Solution {
             }
         }
     }
+
+    float Distance(Coordinate a, Coordinate b) {
+        return sqrt(pow(a.X - b.X, 2) + pow(a.Y - b.Y, 2)) / norm;
+    }
+
+    void OneSeedFlood(Coordinate start, vector<pair<Coordinate, float>>& infobest) {
+        unordered_set<Coordinate, CoordinateHash> q;
+        q.emplace(start);
+        int maxSize = max(imgH, imgW);
+        int stepLength = max(imgH, imgW) / 2;
+        while (stepLength != 0) {
+            unordered_set<Coordinate, CoordinateHash> newQ;
+            newQ.emplace(start);
+            for (const auto& c : q) {
+                for (int i = 0; i < 8; i++) {
+                    Coordinate newC = {c.X + Next8[i][0] * stepLength,
+                                       c.Y + Next8[i][1] * stepLength};
+                    newQ.emplace(newC);
+                    if (newC.X < 0 || newC.X >= imgH) continue;
+                    if (newC.Y < 0 || newC.Y >= imgW) continue;
+                    float dis = Distance(start, newC);
+                    if (dis < infobest[newC.X * imgW + newC.Y].second) {
+                        infobest[newC.X * imgW + newC.Y].first = newC;
+                        infobest[newC.X * imgW + newC.Y].second = dis;
+                    }
+                }
+            }
+            stepLength /= 2;
+            swap(q, newQ);
+        }
+    }
+
+    void JFA() {
+        vector<pair<Coordinate, float>> InfoBest;
+        queue<Coordinate> q;
+        vector<Coordinate> Seeds;
+        int maxSize = max(imgH, imgW);
+        InfoBest.resize(maxSize * maxSize);
+        for (int i = 0; i < imgH; i++) {
+            for (int j = 0; j < imgW; j++) {
+                if (input[i * imgW + j] == 0) {
+                    InfoBest[i * imgW + j] = pair{Coordinate{i, j}, 0.0};
+                    Seeds.emplace_back(Coordinate{i, j});
+                } else {
+                    InfoBest[i * imgW + j] = pair{Coordinate{i, j}, numeric_limits<float>::max()};
+                }
+            }
+        }
+        for (int i = 0; i < Seeds.size(); i++) {
+            OneSeedFlood(Seeds[i], InfoBest);
+        }
+        for (int i = 0; i < imgH; i++) {
+            for (int j = 0; j < imgW; j++) {
+                output[i * imgW + j] = InfoBest[i * imgW + j].second;
+            }
+        }
+    }
 };
 
-
-int main(){
+int main() {
     Solution().Solve();
     return 1;
 }
