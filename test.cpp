ec6a15420270a40ff57304793b7398d9ebbc2b29
WayneWei228 <waynewei0228@gmail.com>
2022-08-25 18:46:07 -0700

    Update

--- test.cpp
+++ test.cpp
@@ -2,6 +2,9 @@
 #include <cstdio>
 using namespace std;
 
+// g++ --std=c++20 -Xpreprocessor -fopenmp -I/opt/homebrew/Cellar/libomp/14.0.6/include -L/opt/homebrew/Cellar/llvm/14.0.6_1/lib -lomp test.cpp 
+
+
 int main() {
 #pragma omp parallel
     { printf("Hello, World\n"); }
