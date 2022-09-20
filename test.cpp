64a76ad5294211444a6aa6a9da025d811c5246ce
WayneWei228 <waynewei0228@gmail.com>
2022-09-20 03:18:03 -0700

    Update

--- test.cpp
+++ test.cpp
@@ -11,24 +11,26 @@
 #define N 1000
 using namespace std;
 using namespace std::chrono;
-const int M = 1000000;
+const int M = 10000000;
 
 int a[M];
 
 int main() {
     int c;
-    freopen("/Users/wayne_tx/Desktop/CG/out.txt", "w", stdout);
+
     for (c = 1; c <= 16; c++) {
-        auto start1 = high_resolution_clock::now();
+        auto start1 = steady_clock::now();
 #pragma omp parallel for schedule(static) num_threads(c)
         for (int i = 0; i < M; i++) {
-            a[i] = (8 * (i + 1) / 19 + 8) / c;
+            for (int j = 0; j < 1000; j++) {
+                a[(i + j) % M] = i;
+            }
         }
-        auto end1 = high_resolution_clock::now();
-        auto ans1 = duration_cast<microseconds>(end1 - start1);
+        auto end1 = steady_clock::now();
+        auto ans1 = duration_cast<milliseconds>(end1 - start1).count();
         printf("Number of Threads : %d\n", c);
-        printf("Run Time : %d microseconds\n\n", ans1);
+        printf("Run Time : %lld milliseconds\n\n", ans1);
     }
-    fclose(stdout);
+
     return 0;
 }
